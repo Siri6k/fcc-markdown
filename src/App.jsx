08f1b032a2eaede209c830f59e5286f21659dd33
sirisk <adamchrisk@gmail.com>
2025-01-15 14:46:09 +0200

    use marked

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { markdown } from "./constants";
-import ReactMarkdown from "react-markdown";
+/*import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
+import rehypeRaw from "rehype-raw";*/
+import { marked } from "marked";
+import fccLogo from "./assets/fcc_secondary_small.png";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -22,8 +24,10 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="d-flex flex-column min-vh-100 min-vw-100 align-items-center justify-content-center">
-        <div className="d-flex flex-column">
-          <label htmlFor="editor">Editor</label>
+        <div className="container d-flex flex-column text-area">
+          <label className="label" htmlFor="editor">
+            Editor
+          </label>
           <textarea
             name="editor"
             value={this.state.markdown}
@@ -31,12 +35,15 @@ export default class App extends React.Component {
             onChange={this.handleChange}
           ></textarea>
         </div>
-        <div id="preview" className="container">
-          <ReactMarkdown
-            children={this.state.markdown}
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeRaw]}
-          />
+        <div className="container d-flex flex-column preview-area">
+          <p className="label">
+            <img src={fccLogo} alt="freeCodeCamp Logo" />
+            Preview
+          </p>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }}
+          ></div>
         </div>
       </div>
     );
